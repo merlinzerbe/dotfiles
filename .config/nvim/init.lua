e04e04d98c7c8421163cbc5596ecaa07a4eca621
Merlin Zerbe <merlin.zerbe@impact-it-services.de>
2026-05-05 08:39:57 +0200

    minor(nvim): enable phpstan and psalm with php-actor

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -491,6 +491,17 @@ local lspconfig_spec = {
       },
     })
 
+    vim.lsp.config('phpactor', {
+      cmd = { 'phpactor', 'language-server' },
+      filetypes = { 'php' },
+      root_markers = { '.git', 'composer.json', '.phpactor.json', '.phpactor.yml' },
+      workspace_required = true,
+      settings = {
+        ["language_server_phpstan.enabled"] = true,
+        ["language_server_psalm.enabled"] = true,
+      }
+    })
+
     vim.lsp.enable({
       "ty",
       "vtsls",
